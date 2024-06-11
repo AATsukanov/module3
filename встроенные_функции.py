03361f88cd830d8221caad656692d88070f90259
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-06-11 12:34:17 +0300

    добавлена практика с функциями и help(), *.__doc__

--- встроенные_функции.py
+++ встроенные_функции.py
@@ -9,6 +9,10 @@
 
 #len(), sum(), min(), max(), round(), zip()
 
+#isinstance(), any(), all()
+
+#type(), id()
+
 salary = [2300, 1800.801234, 5000, 1234.583434, 7500.122323]
 print(round(sum(salary)/len(salary), 2), '- средняя зарплата в компании')
 print(round(max(salary), 2), '- максимальная зарплата в компании')
